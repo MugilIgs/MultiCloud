57e186baa2c5f263ff105f34bb1483645997aca3
MugilIgs <mugilignatius@gmail.com>
2026-08-29 08:44:01 +0530

    add user registration, authentication

---
+++ multicloud-storage-api/multicloud-storage-api/src/main/java/com/multicloud/multicloud_storage_api/MulticloudStorageApiApplication.java
@@ -0,0 +1,13 @@
+package com.multicloud.multicloud_storage_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticloudStorageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulticloudStorageApiApplication.class, args);
+	}
+
+}
---
+++ multicloud-storage-api/multicloud-storage-api/src/main/java/com/multicloud/multicloud_storage_api/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.multicloud.multicloud_storage_api.controller;
+
+
+import com.multicloud.multicloud_storage_api.dto.RegisterRequest;
+import com.multicloud.multicloud_storage_api.entity.User;
+import com.multicloud.multicloud_storage_api.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        userRepository.save(user);
+        return "User registered successfully";
+
+    }
+}
